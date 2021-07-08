849837e8d29e6097a1bfe6249f6df47c5cc223c0
Kevin Sana <kevin.sana@outlook.com>
2021-07-08 08:27:59 -0500

    added user interface

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let playerScore = 0
 let computerScore = 0
+let roundCount = 0
+const results = document.querySelector('#results')
+const playerButtons = document.querySelectorAll('button');
+const playerScoreDisplay = document.querySelector('#player-score')
+const cpuScoreDisplay = document.querySelector('#cpu-score')
+const winner = document.querySelector('#winner')
+
+const disableButtons = () => {
+    playerButtons.forEach((button) => {
+        button.disabled = true;
+    })
+}
 
 const computerPlay = () => {
     let computerChoice = Math.floor(Math.random() * 3)
@@ -13,47 +25,55 @@ const computerPlay = () => {
     }
 }
 
-const playerChoice = string => {
-    if(string === undefined || string === null) {
-        return 'Please enter rock, paper, scissors'
-    } else {
-        return string.toLowerCase()
-    }
-}
-
 const playRound = (playerChoice, computerChoice) => {
     if(playerChoice === 'rock' && computerChoice === 'scissors' ||
         playerChoice === 'paper' && computerChoice === 'rock' ||
         playerChoice === 'scissors' && computerChoice === 'paper'){
             playerScore++
+            roundCount++
             return `Player chooses ${playerChoice} and beats CPU's choice ${computerChoice}`
     } else if 
         (playerChoice === 'rock' && computerChoice === 'paper' ||
         playerChoice === 'paper' && computerChoice === 'scissors' ||
         playerChoice === 'scissors' && computerChoice === 'rock') {
             computerScore++
+            roundCount++
             return `Player chooses ${playerChoice} and loses against CPU's choice ${computerChoice}`
         } else {
+            roundCount++
             return `You both chose ${playerChoice}. It's a tie game.`
         }
 }
 
-const game = () => {
+playerButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        switch (button.id){
+            case 'rock-button':
+                 results.textContent = (playRound('rock', computerPlay()));
+                 playerScoreDisplay.textContent = `Player: ${playerScore}`
+                 cpuScoreDisplay.textContent = `CPU: ${computerScore}`
+                 break;
+            case 'paper-button':
+                 results.textContent = (playRound('paper', computerPlay()));
+                 playerScoreDisplay.textContent = `Player: ${playerScore}`
+                 cpuScoreDisplay.textContent = `CPU: ${computerScore}`
+                 break;
+            case 'scissors-button':
+                 results.textContent = (playRound('scissors', computerPlay()));
+                 playerScoreDisplay.textContent = `Player: ${playerScore}`
+                 cpuScoreDisplay.textContent = `CPU: ${computerScore}`
+                 break;
+        }
+        if(playerScore > computerScore && roundCount === 5) {
+            disableButtons()
+            winner.textContent = `Player wins the Match!`;
+        } else if (computerScore > playerScore && roundCount === 5){
+            disableButtons()
+            winner.textContent = `Computer wins the match.`;
+        } else if(playerScore === computerScore && roundCount === 5) {
+            disableButtons()
+            winner.textContent = `Nobody wins...?`;
+        }
+    })
+})
 
-    for(let i = 0; i < 5; i++){
-        let playerPrompt = prompt(`Rock, Paper, Scissors?`)
-       console.log(playRound(playerChoice(playerPrompt), computerPlay()))
-       console.log(`Player Score: ${playerScore}`)
-       console.log(`Computer Score: ${computerScore}`)
-    }
-
-    if(playerScore > computerScore) {
-        console.log(`Player wins the Match!`)
-    } else if (computerScore > playerScore){
-        console.log(`Computer wins the match.`)
-    } else {
-        console.log(`Nobody wins...?`)
-    }
-}
-
-game()
